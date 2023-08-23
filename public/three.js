dbf9c3bc38b341f45c9c72baacd089676ecb9eb6
SedPC <lilshed3@gmail.com>
2023-08-22 20:26:32 -0700

    Working globe with rotation

--- public/three.js
+++ public/three.js
@@ -14,29 +14,31 @@ const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerH
     1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild( renderer.domElement );
 
 camera.position.setZ(30);
 
-//const sphereGeo = new THREE.SphereGeometry(15, 20, 20);
-const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xE53936, wireframe: true });
-const torus = new THREE.TorusGeometry(10, 3, 16, 100);
+const texture = new THREE.TextureLoader().load("./textures/Ocean_Mask.png");
 
-const sphere = new THREE.Mesh(torus, sphereMaterial);
+const sphereGeo = new THREE.SphereGeometry(15, 30, 30);
+const sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
+const sphere = new THREE.Mesh(sphereGeo, sphereMaterial);
 
 const light = new THREE.PointLight(0xffffff);
-
 light.position.set(20, 20, 20);
 
-scene.add(light);
 scene.add(sphere);
+scene.add(light);
+
+//sphere.rotateX(5);
 
 function render() {
+    sphere.rotateY(0.0015);
+    sphere.rotateX(0.0005);
     renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
